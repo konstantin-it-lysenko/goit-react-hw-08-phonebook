a36d797ffbf6dc658f0224964cca7575ae1cf25d
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-12 22:56:49 +0200

    add App styled

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 335px;
+  padding: 0 20px;
+  @media (min-width: 375px) {
+    max-width: 375px;
+  }
+  @media (min-width: 768px) {
+    max-width: 768px;
+    padding: 0 32px;
+  }
+  @media (min-width: 1440px) {
+    max-width: 1440px;
+  }
+`;
+
+export const MainHeading = styled.h1`
+  margin: 40px 0;
+`;
+
+export const SecondHeading = styled.h2`
+  margin: 40px 0;
+`;
