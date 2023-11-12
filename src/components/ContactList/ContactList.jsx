4735428407fa03bf44300ca687923a861d98bb6b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-12 22:57:34 +0200

    add components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ContactList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ContactList;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class Filter extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Filter;
