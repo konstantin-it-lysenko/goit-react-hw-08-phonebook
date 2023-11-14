3a3ab0098b05d0e911b2d74d1de81d428f70907b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-14 17:18:14 +0200

    complete ContactItem logic

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Contact, ContactName, Number } from './ContactItem.styled';
 
-class ContactItem extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const ContactItem = ({ id, name, number, onDelete }) => {
+  return (
+    <Contact>
+      <ContactName>
+        {name}: <Number>{number}</Number>
+      </ContactName>
+      <IconButton
+        onClick={() => {
+          onDelete(id);
+        }}
+        size="large"
+      >
+        <DeleteIcon fontSize="inherit" style={{ fill: '#fff' }} />
+      </IconButton>
+    </Contact>
+  );
+};
 
 export default ContactItem;
