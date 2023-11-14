2ff134133082239da82fea4b009b87f8d75041ab
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-14 17:18:01 +0200

    add Form styled

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import Box from '@mui/material/Box';
+
+export const StyledFormBox = styled(Box)`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 30px 20px;
+  border: 2px solid #fff;
+  border-radius: 16px;
+  background-color: #302f2f;
+`;
