84b1c8c8146e28cddb2649cdada87c3983de8dcd
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-14 17:19:28 +0200

    add ContactList styled

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+  margin-bottom: 50px;
+  max-width: 344px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 10px;
+`;
