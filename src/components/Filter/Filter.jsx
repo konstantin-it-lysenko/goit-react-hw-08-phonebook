e0dd133eca2faeed41d6972327e9c279d229fd52
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-14 17:19:56 +0200

    add & complete Filter logic

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,26 @@
-import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Container, Icon, Input } from './Filter.styled';
 
-class Filter extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Filter = ({ inputValue, onFilter }) => {
+  const filterId = nanoid();
+  return (
+    <Container>
+      <Input
+        type="text"
+        value={inputValue}
+        id={filterId}
+        onChange={onFilter}
+        label="Find contacts by name"
+        InputProps={{
+          style: { color: 'white' },
+        }}
+        InputLabelProps={{
+          style: { color: 'white' },
+        }}
+      />
+      <Icon fontSize="inherit" />
+    </Container>
+  );
+};
 
 export default Filter;
