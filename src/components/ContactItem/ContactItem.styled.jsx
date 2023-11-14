20103203986ca8087e0cebf19fce2c2953368738
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-14 17:18:26 +0200

    add ContactItem styled

---
+++ src/components/ContactItem/ContactItem.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Contact = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 12px;
+  background-color: #302f2f;
+  border-radius: 12px;
+`;
+
+export const ContactName = styled.p`
+  font-size: 1.07rem;
+`;
+
+export const Number = styled.span`
+  font-weight: 500;
+  color: #358df2;
+`;
