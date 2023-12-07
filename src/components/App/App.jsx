718ea9b93b8ec25812bb5dce1be424143e105175
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-07 18:20:49 +0200

    Refactor class components to functional

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Container } from '@mui/material';
 import ContactForm from 'components/ContactForm/ContactForm';
@@ -6,58 +6,48 @@ import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
 import { MainHeading, SecondHeading } from './App.styled';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const App = () => {
+  const CONTACTS_LS = 'contacts';
 
-  CONTACTS_LS = 'CONTACTS';
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem(CONTACTS_LS)) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const parsedContacts = JSON.parse(localStorage.getItem(this.CONTACTS_LS));
+  // useEffect(() => {
+  //   const parsedContacts = JSON.parse(localStorage.getItem(CONTACTS_LS));
 
-    if (parsedContacts) {
-      this.setState({
-        contacts: parsedContacts,
-      });
-    }
-  }
+  //   if (parsedContacts) {
+  //     setContacts(parsedContacts);
+  //   }
+  // }, []);
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem(
-        this.CONTACTS_LS,
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem(CONTACTS_LS, JSON.stringify(contacts));
+  }, [contacts]);
 
-  formSubmitHandler = ({ name, number }) => {
-    const isDuplicate = this.state.contacts.find(
-      contact => contact.name === name
-    );
+  const formSubmitHandler = ({ name, number }) => {
+    const isDuplicate = contacts.find(contact => contact.name === name);
 
     if (isDuplicate) return alert(`${name} already exists`);
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { name, number, id: nanoid() }],
-    }));
+    setContacts(prev => [...prev, { name, number, id: nanoid() }]);
   };
 
-  filterInputHandler = e => {
-    const inputValue = e.currentTarget.value;
+  const filterInputHandler = e => {
+    const inputValue = e.target.value;
 
-    this.setState({ filter: inputValue });
+    setFilter(inputValue);
   };
 
-  contactFilterHandler = () => {
-    const { contacts, filter } = this.state;
+  const contactFilterHandler = () => {
+    console.log(contacts);
     const valueToLowerCase = filter.toLowerCase().trim();
     const filteredContacts = contacts.filter(contact =>
       contact.name.toLowerCase().trim().includes(valueToLowerCase)
@@ -65,32 +55,27 @@ class App extends Component {
     return filteredContacts;
   };
 
-  deleteContactHandler = filterId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== filterId),
-    }));
+  const deleteContactHandler = filterId => {
+    setContacts(prev => prev.filter(({ id }) => id !== filterId));
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = this.contactFilterHandler();
+  const filteredContacts = contactFilterHandler();
 
-    return (
-      <Container>
-        <MainHeading>Phonebook</MainHeading>
-        <ContactForm onSubmit={this.formSubmitHandler} />
+  return (
+    <Container>
+      <MainHeading>Phonebook</MainHeading>
+      <ContactForm onSubmit={formSubmitHandler} />
 
-        <SecondHeading>Contacts</SecondHeading>
-        <Filter inputValue={filter} onFilter={this.filterInputHandler} />
-        {filteredContacts && (
-          <ContactList
-            contacts={filteredContacts || contacts}
-            onDelete={this.deleteContactHandler}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+      <SecondHeading>Contacts</SecondHeading>
+      <Filter inputValue={filter} onFilter={filterInputHandler} />
+      {filteredContacts && (
+        <ContactList
+          contacts={filteredContacts}
+          onDelete={deleteContactHandler}
+        />
+      )}
+    </Container>
+  );
+};
 
 export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,91 +1,97 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { StyledFormBox } from './ContactForm.styled';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleContactSubmit = e => {
+  const handleContactSubmit = e => {
     e.preventDefault();
 
-    this.props.onSubmit(this.state);
+    onSubmit({ name, number });
 
-    this.reset();
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <StyledFormBox
-        onSubmit={this.handleContactSubmit}
-        component="form"
-        autoComplete="off"
+  return (
+    <StyledFormBox
+      onSubmit={handleContactSubmit}
+      component="form"
+      autoComplete="off"
+    >
+      <TextField
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        label="Name"
+        inputProps={{
+          pattern: "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$",
+          type: 'text',
+          minLength: 2,
+          maxLength: 32,
+        }}
+        required
+        InputProps={{
+          style: { color: 'white' },
+        }}
+        InputLabelProps={{
+          style: { color: 'white' },
+        }}
+      />
+
+      <TextField
+        name="number"
+        value={number}
+        onChange={handleChange}
+        label="Phone number"
+        required
+        inputProps={{
+          pattern:
+            '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}',
+          type: 'tel',
+          minLength: 10,
+        }}
+        InputProps={{
+          style: { color: 'white' },
+        }}
+        InputLabelProps={{
+          style: { color: 'white' },
+        }}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{
+          width: 300,
+          height: 50,
+        }}
       >
-        <TextField
-          type="text"
-          name="name"
-          value={this.state.name}
-          onChange={this.handleChange}
-          label="Name"
-          inputProps={{
-            pattern:
-              "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$",
-            type: 'text',
-            minLength: 2,
-            maxLength: 32,
-          }}
-          required
-          InputProps={{
-            style: { color: 'white' },
-          }}
-          InputLabelProps={{
-            style: { color: 'white' },
-          }}
-        />
+        Add contact
+      </Button>
+    </StyledFormBox>
+  );
+};
 
-        <TextField
-          name="number"
-          value={this.state.number}
-          onChange={this.handleChange}
-          label="Phone number"
-          required
-          inputProps={{
-            pattern:
-              '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}',
-            type: 'tel',
-            minLength: 10,
-          }}
-          InputProps={{
-            style: { color: 'white' },
-          }}
-          InputLabelProps={{
-            style: { color: 'white' },
-          }}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          sx={{
-            width: 300,
-            height: 50,
-          }}
-        >
-          Add contact
-        </Button>
-      </StyledFormBox>
-    );
-  }
-}
 export default ContactForm;
