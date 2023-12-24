451b3113e7be490d91bb894645915a449bdea594
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-25 00:36:20 +0200

    refactor ContactItem logic, replace with reducer

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,8 +1,16 @@
+import { useDispatch } from 'react-redux';
+import { deleteContact } from './../../redux/contactsSlice/contactsSlice';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Contact, ContactName, Number } from './ContactItem.styled';
 
-const ContactItem = ({ id, name, number, onDelete }) => {
+const ContactItem = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+
+  const deleteContactHandler = filterId => {
+    dispatch(deleteContact(filterId));
+  };
+
   return (
     <Contact>
       <ContactName>
@@ -10,7 +18,7 @@ const ContactItem = ({ id, name, number, onDelete }) => {
       </ContactName>
       <IconButton
         onClick={() => {
-          onDelete(id);
+          deleteContactHandler(id);
         }}
         size="large"
       >
