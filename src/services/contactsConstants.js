d46bc964e6a219cdbd2f9945416078795040c4bb
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-27 22:49:45 +0200

    add axios constants and endpoint

---
+++ src/services/contactsConstants.js
@@ -0,0 +1,6 @@
+export const contactsApi = Object.freeze({
+    BASE_URL: 'https://658b059bba789a9622385157.mockapi.io/api',
+    ENDPOINTS: Object.freeze({
+        CONTACTS: '/contacts',
+    })
+})
