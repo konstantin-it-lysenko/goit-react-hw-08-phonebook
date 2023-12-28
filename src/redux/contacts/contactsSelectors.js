aecefecab750087c4fcd2002ae9f469dffa8275d
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-28 16:08:30 +0200

    create contacts selectors

--- src/redux/contacts/contactsSelectors.js
+++ src/redux/contacts/contactsSelectors.js
@@ -1,1 +1,3 @@
 export const selectContacts = ({ contacts }) => contacts.items;
+export const selectLoading = ({ contacts }) => contacts.isLoading;
+export const selectError = ({ contacts }) => contacts.error;
