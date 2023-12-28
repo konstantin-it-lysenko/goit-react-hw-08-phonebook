57d3cebea9ae243727eaf19d978abb71b61233d6
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-28 16:06:58 +0200

    add useEffect for getContacts, loader / move delete contact logic from contactItem

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,39 +1,63 @@
-import { useSelector } from 'react-redux';
-import { selectContacts } from '../../redux/contacts/contactsSelectors';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getContacts } from '../.././redux/contacts/contactsOperations';
+import { deleteContact } from '../../redux/contacts/contactsOperations';
+import {
+  selectContacts,
+  selectLoading,
+  selectError,
+} from '../../redux/contacts/contactsSelectors';
 import { selectFilterValue } from '../../redux/filter/filterSelectors';
 import ContactItem from 'components/ContactItem/ContactItem';
 import { List } from './ContactList.styled';
+import Loader from 'components/Loader/Loader';
 
 const ContactList = () => {
+  const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
   const filter = useSelector(selectFilterValue);
+
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
 
   const filterContacts = () => {
     const normalizedFilter = filter.toLowerCase().trim();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
+
+    return contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(normalizedFilter);
+    });
   };
 
   const filteredContacts = filterContacts();
+  const isListEmpty = !filteredContacts.length && !isLoading && !isError;
+
+  const deleteContactHandler = filterId => {
+    dispatch(deleteContact(filterId));
+  };
 
   return (
-    <List>
-      {!filteredContacts.length ? (
-        <p>There are no contacts</p>
-      ) : (
-        filteredContacts.map(({ id, name, number }) => {
-          return (
-            <ContactItem
-              key={id}
-              id={id}
-              name={name}
-              number={number}
-            ></ContactItem>
-          );
-        })
+    <>
+      {isLoading && <Loader />}
+      {isListEmpty && <p>There are no contacts</p>}
+      {filteredContacts.length > 0 && (
+        <List>
+          {filteredContacts.map(({ id, name, phone }) => {
+            return (
+              <ContactItem
+                key={id}
+                id={id}
+                name={name}
+                number={phone}
+                onDelete={deleteContactHandler}
+              ></ContactItem>
+            );
+          })}
+        </List>
       )}
-    </List>
+    </>
   );
 };
 
