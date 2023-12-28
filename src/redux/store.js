4c956b25957cda635599899e8639bddf8f419589
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-28 15:52:46 +0200

    remove persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import { persistedContactsReducer } from "./contacts/contactsSlice";
+import { contactsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./filter/filterSlice";
 
 export const store = configureStore({
     reducer: {
-        contacts: persistedContactsReducer,
+        contacts: contactsReducer,
         filter: filterReducer,
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
 });
 
-export const persistor = persistStore(store);
+
