4bf6db50bed92a61c275d4a53de39b75f7806713
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-28 16:08:20 +0200

    update operations with axios instance

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,38 +1,34 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
 import { instance } from "services/contactsApi";
 import { contactsApi } from "services/contactsConstants";
 
-export const fetchContactsThunk = createAsyncThunk('contacts/getContacts', async (_, thunkAPI) => {
+export const getContacts = createAsyncThunk('contacts/getContacts', async (_, thunkAPI) => {
     try {
         const { data } = await instance.get(contactsApi.ENDPOINTS.CONTACTS);
 
         return data
     } catch (err) {
-        console.error(err)
         thunkAPI.rejectWithValue(err)
     }
 
 })
 
-export const addContactThunk = createAsyncThunk('contacts/addContact', async (contact, thunkAPI) => {
+export const addContact = createAsyncThunk('contacts/addContact', async (contact, thunkAPI) => {
     try {
-        const { data } = await axios.post(contactsApi.ENDPOINTS.CONTACTS, contact);
+        const { data } = await instance.post(contactsApi.ENDPOINTS.CONTACTS, contact);
 
         return data
     } catch (err) {
-        console.error(err)
         thunkAPI.rejectWithValue(err)
     }
 })
 
-export const deleteContactThunk = createAsyncThunk('contacts/deleteContact', async (id, thunkAPI) => {
+export const deleteContact = createAsyncThunk('contacts/deleteContact', async (id, thunkAPI) => {
     try {
-        const { data } = await axios.delete(`${contactsApi.ENDPOINTS.CONTACTS}/${id}`,);
+        const { data } = await instance.delete(`${contactsApi.ENDPOINTS.CONTACTS}/${id}`,);
 
         return data
     } catch (err) {
-        console.error(err)
         thunkAPI.rejectWithValue(err)
     }
 })
