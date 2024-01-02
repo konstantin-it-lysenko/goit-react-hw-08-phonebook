d1a19e8be5b6acf2d7dbc91158fc369a6ab12872
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-02 21:46:03 +0200

    remove services

--- src/services/contactsApi.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { contactsApi } from "./contactsConstants";
-
-export const instance = axios.create({
-    baseURL: contactsApi.BASE_URL,
-})
--- src/services/contactsConstants.js
+++
@@ -1,6 +0,0 @@
-export const contactsApi = Object.freeze({
-    BASE_URL: 'https://658b059bba789a9622385157.mockapi.io/api',
-    ENDPOINTS: Object.freeze({
-        CONTACTS: '/contacts',
-    })
-})
