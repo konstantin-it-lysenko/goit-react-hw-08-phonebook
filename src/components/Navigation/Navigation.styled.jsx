e7e1dd91e6bcb6c2476631700f5c30a4821c2ef1
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-02 21:55:19 +0200

    add Navigation styled

---
+++ src/components/Navigation/Navigation.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const NavMenu = styled.nav`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  gap: 30px;
+`;
