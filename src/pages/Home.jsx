e50255188c64193e7a488db0fdf6665ad4cad9fd
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-02 21:49:25 +0200

    create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Phonebook App</h1>
+      <p>Register to add and save contacts to your phonebook</p>
+    </div>
+  );
+};
+
+export default Home;
