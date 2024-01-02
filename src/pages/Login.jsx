63c25cf19aa0a5b84c342a4dc51e85aba660ffa8
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-02 21:49:59 +0200

    create login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import LoginForm from 'components/LoginForm/LoginForm';
+import React from 'react';
+
+const Login = () => {
+  return (
+    <>
+      <h2>Log In</h2>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
