035df9aab663e7c6fe0dd491cab1e40e16f0dc3c
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-02 21:55:37 +0200

    add shared layout styled

---
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  background: #000;
+  padding: 20px 0;
+`;
+
+export const HeaderWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 1540px;
+`;
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 1540px;
+`;
