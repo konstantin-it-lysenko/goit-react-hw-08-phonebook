8520669daaa7857883c7dd2587bd6f182e4269ce
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-02 21:48:51 +0200

    create routes / get token data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,32 @@
-import ContactForm from 'components/ContactForm/ContactForm';
-import Filter from 'components/Filter/Filter';
-import ContactList from 'components/ContactList/ContactList';
-import { Container } from '@mui/material';
-import { MainHeading, SecondHeading } from './App.styled';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from '../../redux/auth/authOperations';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from 'components/SharedLayout/SharedLayout';
+import Home from 'pages/Home';
+import Register from 'pages/Register';
+import Login from 'pages/Login';
+import Contacts from 'pages/Contacts';
 
 const App = () => {
-  return (
-    <Container>
-      <MainHeading>Phonebook</MainHeading>
-      <ContactForm />
+  const dispatch = useDispatch();
 
-      <SecondHeading>Contacts</SecondHeading>
-      <Filter />
-      <ContactList />
-    </Container>
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="*" element={<div>Page not found</div>} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
