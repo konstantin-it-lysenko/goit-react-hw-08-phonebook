9c0c8cf382632cd75c044b3ed3222e8cc0f9a7d6
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-03 21:08:52 +0200

    add refresh and user selectors

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,2 +1,4 @@
 export const selectUsername = ({ auth }) => auth.user.name;
+export const selectUser = ({ auth }) => auth.user;
 export const selectLoggedIn = ({ auth }) => auth.isLoggedIn;
+export const selectRefreshing = ({ auth }) => auth.isRefreshing;
